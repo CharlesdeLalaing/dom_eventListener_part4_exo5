61b7d7eb15f54f603191e56a2da35901258c9258
charles de Lalaing <71701904+CharlesdeLalaing@users.noreply.github.com>
2021-05-18 14:27:29 +0200

    push correction

--- public/js/main.js
+++ public/js/main.js
@@ -63,10 +63,74 @@ sourceImage2.addEventListener('click',() => {
 
 let exo7text1 = document.getElementsByTagName('p')[5];
 let exo7text2 = document.getElementsByTagName('p')[6];
-let temp = exo7text2
 
 let exo7btn = document.getElementsByClassName('btn btn-primary mb-2')[1];
 
 exo7btn.addEventListener('click',()=>{
     [exo7text1.innerHTML, exo7text2.innerHTML] = [exo7text2.innerHTML, exo7text1.innerHTML]
+});
+
+
+//correction
+
+//exo1
+let exo1correction = document.getElementsByTagName('input')[0];
+exo1correction.addEventListener('focus',()=>{
+    exo1correction.setAttribute('style', 'background-color:blue');
+});
+
+//exo2
+let exo2correction = document.getElementsByTagName('input')[1];
+exo2correction.addEventListener('focus',()=>{
+    exo2correction.setAttribute('style', 'background-color:blue');
+});
+exo2correction.addEventListener('focusout',()=>{
+    exo2correction.setAttribute('style', 'background-color:white');
+});
+
+//exo3
+let paragraphe0 = document.getElementsByClassName('premierParagraphe')[0];
+let paragraphe1 = document.getElementsByClassName('secondParagraphe')[0];
+let paragraphe2 = document.getElementsByClassName('dernierParagraphe')[0];
+let bouton0 = document.getElementsByClassName('buttonExo')[0];
+bouton0.addEventListener('click',()=>{
+    paragraphe0.innerHTML = paragraphe1.innerHTML;
+    paragraphe2.innerHTML = paragraphe1.innerHTML;
+});
+
+//exo4
+let input3 = document.querySelectorAll('input')[2];
+let txt = document.querySelector('#exo4');
+let bouton1 = document.getElementsByClassName('buttonExo')[1];
+bouton1.addEventListener('click', () => {
+    txt.innerHTML = input3.value;
 })
+
+//exo5
+let bouton2 = document.querySelectorAll('button')[2];
+let img1 = document.querySelectorAll('img')[0];
+let pChemin = img1.previousElementSibling.innerText;
+
+bouton2.addEventListener('click',()=>{
+    img1.src = pChemin.substr(9);
+});
+
+//exo6
+let img2 = document.querySelectorAll('img')[1];
+let img3 = document.querySelectorAll('img')[2];
+
+img2.addEventListener('click',()=>{
+    img3.src = img2.src;
+});
+
+//exo7
+let btn3 =  document.getElementsByClassName('btn')[3];
+let paragraphe3 = document.querySelectorAll('p')[5];
+let paragraphe4 = document.querySelectorAll('p')[6];
+
+let tempo;
+btn3.addEventListener('click',()=>{
+    tempo = paragraphe2.innerText;
+    paragraphe4.innerText = paragraphe3.innerText;
+    paragraphe3.innerText = tempo;
+});
